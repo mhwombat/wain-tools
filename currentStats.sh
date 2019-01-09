71830d253dc7bb220348a85e5ead1eb5b77a4f37
Amy de Buitléir <amy@nualeargais.ie>
2019-01-09 16:52:19 +0000

    renamed scripts

--- currentStats.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-grep 'Summary - avg' ${log} | tail -n 1 | sed 's/,/\n/g'
--- snapshot.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-source ./config.sh
-grep WARN $log
-./quickSummary.sh | head-tail 2 10 ; ./energySummary.sh | head-tail 2 10
